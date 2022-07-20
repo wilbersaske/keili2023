8335d5cc5425791b057c7eb20207936d82699f05
wilbersaske <108158971+wilbersaske@users.noreply.github.com>
2022-07-20 18:32:46 +0800

    Update fun-presentate.js

--- plugins/fun-presentate.js
+++ plugins/fun-presentate.js
@@ -3,7 +3,7 @@ let vn = './media/presentate.mp3'
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/glass', {
 avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-}), 'error.png', '*🦠〄᭪๑ꪳ⿻ཻ࿔ꪾ✩Welcome!࿑⃕⃰ꨃ᭡⏑ི᭬۵᭪*', m)
+}), 'error.png', '*🦠〄᭪๑ꪳ⿻ཻ࿔ꪾ✩Welcome!࿑⃕⃰ꨃ᭡⏑ི᭬۵᭪ Tanto si usas PowerPoint como Keynote, la tipografía es el elemento esencial para conseguir una presentación fluida, legible e impactante. Las letras te ayudarán a mejorar la comunicación de tus ideas. Te recomendamos cinco tipos de letra sin remate que funcionan muy bien tanto en pantalla como para imprimir posteriormente la presentación. La selección es una mezcla de tipografías muy potentes con rasgos y detalles muy interesantes:   *', m)
 await await await conn.sendFile(m.chat, vn, 'error.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true })
