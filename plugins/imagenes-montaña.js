b4b87c376a162d52e9a0708af962473308db5acc
wilbersaske <108158971+wilbersaske@users.noreply.github.com>
2022-07-21 03:46:03 +0800

    Update imagenes-montaña.js

--- plugins/imagenes-montaña.js
+++ plugins/imagenes-montaña.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, command }) => {
     let yh = global.montaña
     let url = yh[Math.floor(Math.random() * yh.length)]
-    conn.sendButton(m.chat, `FONDOS DE PANTALLA - MONTAÑAS 🥺`.trim(), author, url, [['🌷 𝐒 𝐈 𝐆 𝐔 𝐈 𝐄 𝐍 𝐓 𝐄 🌷 ', `/${command}`]], m)
+    conn.sendButton(m.chat, `FONDOS DE PANTALLA - 🥺`.trim(), author, url, [['🌷 𝐒 𝐈 𝐆 𝐔 𝐈 𝐄 𝐍 𝐓 𝐄 🌷 ', `/${command}`]], m)
   }
   handler.command = /^(montaña)$/i
   handler.tags = ['montaña']
