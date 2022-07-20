01b346553d47df36e38c3ed8f38cb1c2c15b122e
wilbersaske <108158971+wilbersaske@users.noreply.github.com>
2022-07-20 04:10:01 -0500

    Add files via upload

---
+++ plugins/fun-presentate.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn }) => {
+let vn = './media/presentate.mp3'
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/glass', {
+avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+}), 'error.png', '*🏳😁🦠FICHA DE PRESENTACION😁🦠*', m)
+await await await conn.sendFile(m.chat, vn, 'error.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true })
+}
+handler.help = ['presentate']
+handler.tags = ['maker']
+handler.command = /^(presentate)$/i
+export default handler
