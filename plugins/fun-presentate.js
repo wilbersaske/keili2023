81e579950ab22c1799f51cd7abf11ffbc8938402
wilbersaske <108158971+wilbersaske@users.noreply.github.com>
2022-07-20 17:42:14 +0800

    Update fun-presentate.js

--- plugins/fun-presentate.js
+++ plugins/fun-presentate.js
@@ -3,7 +3,35 @@ let vn = './media/presentate.mp3'
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/glass', {
 avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-}), 'error.png', '*🏳😁🦠FICHA DE PRESENTACION😁🦠*', m)
+}), 'error.png', '*🏳😁🦠FICHA DE PRESENTACION ╾┉╌═╼╌┉╾┉╌═╼╌┉╾┉╌╮
+
+〄᭪๑ꪳ⿻ཻ࿔ꪾ✩Welcome!࿑⃕⃰ꨃ᭡⏑ི᭬۵᭪║
+
+╭┉╌═╼╌┉╾┉╌═╼╌┉╾┉╌╯
+
+║䩲䩼▩ꪳ❀ཽ࿔⃨⃕๑̫♡ི༘༗ཻᮭ⊱#TokyoGang (❁´◡`❁)
+
+╰┉╌═╼╌┉╾┉╌═╼╌┉╾┉╌┉╌═╼╌┉╾┉╌╮
+
+[Imagen de tu personaje]
+
+▓꙰꙰꙰꙰꙰⃝⃝⃝⃝⃝ :sweat_drops: ⃢⃢⃢⃢⃢⵿꙰꙰⃟⃟࿑⃟⃟⃟⃟⃟⃝⃝⃝⃝⃝⃝꙰꙰꙰꙰꙰꙰꙰⋱꙰꙰꙰꙰ (Iɴғᴏʀᴍᴀᴄɪᴏ́ɴ ʙᴀ́sɪᴄᴀ) ▓꙰꙰꙰꙰꙰⃝⃝⃝⃝⃝ :sweat_drops: ⃢⃢⃢⃢⃢⵿꙰꙰⃟⃟࿑⃟⃟⃟⃟⃟⃝⃝⃝⃝⃝⃝꙰꙰꙰꙰꙰꙰꙰⋱꙰꙰꙰꙰
+
+╭╦╮⋱⋱╭╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╮
+
+╠╬╣▓꙰꙰꙰꙰꙰⵿᳘ྃྃྃ꙰ꦿ╏⵿ཽི᳗᷍꙰ཹ⵿ཽ᳘ Nᴏᴍʙʀᴇ:
+
+╠╬╣▓꙰꙰꙰꙰꙰⵿᳘ྃྃྃ꙰ꦿ╏⵿ཽི᳗᷍꙰ཹ⵿ཽ᳘ Edɑd:
+
+╠╬╣▓꙰꙰꙰꙰꙰⵿᳘ྃྃྃ꙰ꦿ╏⵿ཽི᳗᷍꙰ཹ⵿ཽ᳘ Gᴇ́ɴᴇʀᴏ:
+
+╠╬╣▓꙰꙰꙰꙰꙰⵿᳘ྃྃྃ꙰ꦿ╏⵿ཽི᳗᷍꙰ཹ⵿ཽ᳘ Oʀɪᴇɴᴛᴀᴄɪᴏ́ɴ sᴇxᴜᴀʟ:
+
+╠╬╣▓꙰꙰꙰꙰꙰⵿᳘ྃྃྃ꙰ꦿ╏⵿ཽི᳗᷍꙰ཹ⵿ཽ᳘ Aᴘᴏᴅᴏ:
+
+╰╩╯⋱⋱╰╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╯
+
+😁🦠*', m)
 await await await conn.sendFile(m.chat, vn, 'error.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true })
