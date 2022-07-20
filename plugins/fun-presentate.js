3c75341b05a136d4827f8152c3151a251da4586f
wilbersaske <108158971+wilbersaske@users.noreply.github.com>
2022-07-20 18:47:37 +0800

    Update fun-presentate.js

--- plugins/fun-presentate.js
+++ plugins/fun-presentate.js
@@ -3,13 +3,7 @@ let vn = './media/presentate.mp3'
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/glass', {
 avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-}), 'error.png', '*🦠〄᭪๑ꪳ⿻ཻ࿔ꪾ✩Welcome!࿑⃕⃰ꨃ᭡⏑ི᭬۵᭪.....................
-'nombre'
-'........................'
-'edad' 
-'........................'
-'Prueba de presentacion'
-'.......................'  *', m)
+}), 'error.png', '*🦠〄᭪๑ꪳ⿻ཻ࿔ꪾ✩Welcome!࿑⃕⃰ꨃ᭡⏑ི᭬۵᭪.....................nombre........................edad........................Prueba de presentacion ......................'  *', m)
 await await await conn.sendFile(m.chat, vn, 'error.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true })
