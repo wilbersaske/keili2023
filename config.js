b1d9d48e906e26c7e4164ffb4af201ca64f11119
wilbersaske <108158971+wilbersaske@users.noreply.github.com>
2022-08-01 11:30:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -77,11 +77,7 @@ global.imagen2 = fs.readFileSync('./src/nuevobot.jpg')
 global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png')
 
 global.multiplier = 9999 
-global.wil = {
-'Númeroone',
-'Menu0',
-'Minyu1',
-'Minyuw2',
+global.wil = {'Númeroone','Menu0','Minyu1','Minyuw2',
 'Minyuw3',
 'Minyu4',
 'Praa',
